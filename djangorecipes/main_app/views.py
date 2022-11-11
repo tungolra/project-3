deb4a70f2dfabad4aba80a4f2c62a2ea1a2cdd35
rtungol <rarrtungol@gmail.com>
2022-11-10 23:41:14 -0500

    django authentication/oauth set up

--- djangorecipes/main_app/views.py
+++ djangorecipes/main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
@@ -8,3 +8,17 @@ from django.views import generic
 
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = "Invalid sign up - try again"
+    form = UserCreationForm
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)

0ae04c81854a03ac91adfab1c249cf61d1572f4e
SK <sk@debian-BULLSEYE-live-builder-AMD64>
2022-11-10 21:02:11 -0500

    Enter notes for environ usage into settings

--- djangorecipes/djangorecipes/settings.py
+++ djangorecipes/djangorecipes/settings.py
@@ -11,9 +11,23 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Set up environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+### Notes on django-environ module
+
+# Steps to install django-environ
+# pip install django-environ
+
+# Get a value from .env
+# MY_KEY = env('PARAMETER_NAME')
+
 
 
 # Quick-start development settings - unsuitable for production

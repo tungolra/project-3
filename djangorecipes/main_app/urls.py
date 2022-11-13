a63d4bc057e6dec36c743d326ab35919c0f326d6
Lucas <lucas.friedmann35@live.ca>
2022-11-12 22:46:45 -0500

    edit recipe details path and view funtion

--- djangorecipes/main_app/urls.py
+++ djangorecipes/main_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('meal-plans', views.meal_plan_index, name='meal_plan_index'),
     path('meal-plans/new', views.meal_plan_new, name='meal_plan_new'),
     path('meal-plans/create', views.meal_plan_create, name='meal_plan_create'),
-    path('recipe', views.recipe_view, name="recipe_view"),
+    path('recipes/', views.recipe_view, name="recipe_view"),
     path('example', views.example, name="example"),
 ]

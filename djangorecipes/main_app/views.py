a59e9eb8cb4bd701e9fdb2f7131d98f7236a79c6
Lucas <lucas.friedmann35@live.ca>
2022-11-13 17:43:06 -0500

    update details page with new divs

--- djangorecipes/main_app/views.py
+++ djangorecipes/main_app/views.py
@@ -81,11 +81,12 @@ def signup(request):
 
 def recipe_view(request):
     p = {
-        "id":"8138" #REQUIRED
+        "id":"7324" #REQUIRED
     }
 
     response = tc_api.client.get_recipes_details(p)
     data = utils.parse_recipes_details(response, "d")
+    print(data)
     return render(request, "recipes/details.html", {"recipe":data})
 
 

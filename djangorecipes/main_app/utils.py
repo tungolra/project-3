173ec24fa39d99c0488addc03df6952c8af9c39d
Lucas <lucas.friedmann35@live.ca>
2022-11-13 22:20:03 -0500

    update recipe_summary

--- djangorecipes/main_app/utils.py
+++ djangorecipes/main_app/utils.py
@@ -37,6 +37,7 @@ def parse_recipe_summary(recipe):
     result = {
         "id" : recipe["id"],
         "name": recipe["name"],
+        "time": recipe["cook_time_minutes"],
         "num_servings" : recipe["num_servings"],
         "rating" : recipe["user_ratings"], 
         "image_url" : recipe["thumbnail_url"],
